5a8eafb27a176932732996a6a22bfe0e70747633
nathan-rr-mello <nathanreismello2@gmail.com>
2025-09-18 23:47:04 -0300

    bring experiments to container

--- scripts/deploy.cloudlab.sh
+++ scripts/deploy.cloudlab.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Usage: ./deploy.cloudlab.sh <config_file> <cue_file>
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <config_file> <cue_file>"
+    exit 1
+fi
+
+CONFIG_FILE="$1"
+CUE_FILE="$2"
+
 join() {
 	local IFS="$1"
 	shift
@@ -18,7 +27,7 @@ compose_args="--project-name=hotstuff"
 
 docker compose $compose_args -f docker-compose.cloudlab.yml up -d --build controller
 docker compose $compose_args exec -T controller /bin/sh -c "ssh-keyscan -H $(join ' ' "${hosts[@]}") >> ~/.ssh/known_hosts" &>/dev/null
-docker compose $compose_args exec -T controller /bin/sh -c "hotstuff run --config=./example_config.yml --ssh-config=.ssh/config --exe=/usr/local/hotstuff/hotstuff --cue=./host_config.cue"
+docker compose $compose_args exec -T controller /bin/sh -c "hotstuff run --config=$CONFIG_FILE --ssh-config=.ssh/config --exe=/usr/local/hotstuff/hotstuff --cue=$CUE_FILE"
 exit_code="$?"
 
 
